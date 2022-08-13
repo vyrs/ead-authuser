bd50df65c22a8585499be96f2f64791b48e80ae4
vitor <vitory.rsilva@hotmail.com>
2022-08-13 19:22:23 -0300

    Circuit Breaker

--- src/main/kotlin/com/ead/authuser/clients/CourseClient.kt
+++ src/main/kotlin/com/ead/authuser/clients/CourseClient.kt
@@ -5,16 +5,18 @@ import com.ead.authuser.configs.log
 import com.ead.authuser.dtos.CourseDto
 import com.ead.authuser.dtos.ResponsePageDto
 import com.ead.authuser.services.UtilsService
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import org.springframework.web.client.HttpStatusCodeException
 import org.springframework.web.client.RestTemplate
-import java.util.UUID
+import java.util.*
 
 
 @Component
@@ -22,6 +24,9 @@ class CourseClient(private val restTemplate: RestTemplate, private val utilsServ
 
     @Value("\${ead.api.url.course}")
     lateinit var REQUEST_URL_COURSE: String
+
+//    @Retry(name = "retryInstance", fallbackMethod = "retryfallback")
+    @CircuitBreaker(name = "circuitbreakerInstance")
     fun getAllCoursesByUser(userId: UUID, pageable: Pageable): Page<CourseDto> {
         var result: ResponseEntity<ResponsePageDto<CourseDto>>? = null
         val url = REQUEST_URL_COURSE + utilsService.createUrlGetAllCoursesByUser(userId, pageable)
@@ -38,5 +43,17 @@ class CourseClient(private val restTemplate: RestTemplate, private val utilsServ
         }
         log().info("Ending request /courses userId {} ", userId)
         return result!!.body!!
+    }
+
+    fun circuitbreakerfallback(userId: UUID, pageable: Pageable, t: Throwable): Page<CourseDto> {
+        log().error("Inside circuit breaker fallback, cause - {}", t.toString())
+        val searchResult: List<CourseDto> = ArrayList()
+        return PageImpl(searchResult)
+    }
+
+    fun retryfallback(userId: UUID, pageable: Pageable, t: Throwable): Page<CourseDto> {
+        log().error("Inside retry retryfallback, cause - {}", t.toString())
+        val searchResult: List<CourseDto> = ArrayList()
+        return PageImpl(searchResult)
     }
 }
--- src/main/kotlin/com/ead/authuser/configs/RestTemplateConfig.kt
+++ src/main/kotlin/com/ead/authuser/configs/RestTemplateConfig.kt
@@ -5,14 +5,20 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
+import java.time.Duration
 
 @Configuration
 class RestTemplateConfig {
+
+    val TIMEOUT = 5000L
 
     @LoadBalanced
     @Bean
     fun restTemplate(builder: RestTemplateBuilder): RestTemplate {
         // Do any additional configuration here
-        return builder.build()
+        return builder
+            .setConnectTimeout(Duration.ofMillis(TIMEOUT))
+            .setReadTimeout(Duration.ofMillis(TIMEOUT))
+            .build()
     }
 }
