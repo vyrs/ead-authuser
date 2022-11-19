6461257a9305f1ba09b7ade720f24025e29e95ba
vitor <vitory.rsilva@hotmail.com>
2022-11-19 20:59:48 -0300

    insert create admin endpoint

--- src/main/kotlin/com/ead/authuser/controllers/AuthenticationController.kt
+++ src/main/kotlin/com/ead/authuser/controllers/AuthenticationController.kt
@@ -8,6 +8,7 @@ import com.ead.authuser.dtos.LoginDto
 import com.ead.authuser.dtos.UserDto
 import com.ead.authuser.dtos.toModel
 import com.ead.authuser.enums.RoleType
+import com.ead.authuser.enums.UserType
 import com.ead.authuser.services.RoleService
 import com.ead.authuser.services.UserService
 import com.fasterxml.jackson.annotation.JsonView
@@ -53,7 +54,7 @@ class AuthenticationController(
 
         val passwordEncoded = passwordEncoder.encode(userDto.password)
 
-        val userModel = userDto.toModel(roleModel, passwordEncoded)
+        val userModel = userDto.toModel(roleModel, passwordEncoded, UserType.STUDENT)
         userService.saveUser(userModel)
         log().debug("POST registerUser userId saved {} ", userModel.userId)
         log().info("User saved successfully userId {} ", userModel.userId)
@@ -69,5 +70,31 @@ class AuthenticationController(
         val jwt: String = jwtProvider.generateJwt(authentication)
 
         return ResponseEntity.ok(JwtDto(jwt))
+    }
+
+    @PostMapping("/signup/admin/usr")
+    fun registerUserAdmin(@RequestBody @Validated(UserDto.UserView.RegistrationPost::class) @JsonView(UserDto.UserView.RegistrationPost::class) userDto: UserDto): ResponseEntity<Any> {
+        log().debug("POST registerUser userDto received {} ", userDto.toString())
+
+        if(userService.existsByUsername(userDto.username!!)){
+            log().warn("Username {} is Already Taken ", userDto.username)
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is Already Taken!")
+        }
+
+        if(userService.existsByEmail(userDto.email!!)){
+            log().warn("Email {} is Already Taken ", userDto.email)
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Email is Already Taken!")
+        }
+
+        val roleModel = roleService.findByRoleName(RoleType.ROLE_ADMIN)
+            .orElseThrow { RuntimeException("Error: Role is Not Found.") }
+
+        val passwordEncoded = passwordEncoder.encode(userDto.password)
+
+        val userModel = userDto.toModel(roleModel, passwordEncoded, UserType.ADMIN)
+        userService.saveUser(userModel)
+        log().debug("POST registerUser userId saved {} ", userModel.userId)
+        log().info("User saved successfully userId {} ", userModel.userId)
+        return ResponseEntity.status(HttpStatus.CREATED).body(userModel)
     }
 }
